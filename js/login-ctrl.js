b04b15c60cdd50d6ebbb7043f20c3ccc39eb12b0
seb <seb.chevre>
2017-04-18 20:42:57 +0200

    merge init work by sebchevre

--- js/login-ctrl.js
+++ js/login-ctrl.js
@@ -1,9 +1,15 @@
-angular.module('app').controller('LoginCtrl', function(AuthService, $http, $state) {
+angular.module('app').controller('LoginCtrl',function(AuthService, $http, $state,notify) {
   var login = this;
-
+  var notificationMsg  = '';
+  var notificationMsgCssClasses = [];
+  
   login.user = {};
+  
+  
 
   login.connect = function() {
+	  console.log(loginForm.password.$valid)
+	  
   	delete login.error;
     console.log(login.user);
 	  $http({
@@ -12,10 +18,29 @@ angular.module('app').controller('LoginCtrl', function(AuthService, $http, $stat
 	    data: login.user
 	  }).then(function(res) {
 	    AuthService.setToken(res.data.token);
+        notificationMsg  = 'Connexion effectué avec succès!';
+		otificationMsgCssClasses = [];
+        notificationMsgCssClasses.push('alert-success');
+		notify({message:notificationMsg,duration:3000,classes:notificationMsgCssClasses});
 	    $state.go('home');
 	  }).catch(function(error) {
   		login.error = "Unable to log you."
+         notificationMsg  = 'Login et/ou mot de passe érroné';
+		 otificationMsgCssClasses = [];
+         notificationMsgCssClasses.push('alert-danger');
+		 notify({message:notificationMsg,duration:3000,classes:notificationMsgCssClasses});
     	console.log(error);
+		clearFields();
   	  });    
+	  
   };
+  
+  var clearFields = function () {
+      login.user = {};
+	  delete login.error;
+	  
+	  //$scope.password = '';
+      //$scope.login = '';
+  }
+  
 });
