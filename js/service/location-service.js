6f247d180241b2429bb31187d7bc314aadff84f1
seb <seb.chevre>
2017-05-16 12:48:16 +0200

    before checkout geoloc

--- js/service/location-service.js
+++ js/service/location-service.js
@@ -7,10 +7,24 @@ app.factory('LocationService', function (store, $geolocation) {
         longitude: store.get('longitude'),
 
         setLocation: function (position) {
-            service.latitude = position.coords.latitude;
-            service.longitude = position.coords.longitude;
-            store.set('latitude', service.latitude);
-            store.set('longitude', service.longitude);
+            console.log("location passed:")
+            console.log(position);
+            console.log($geolocation.position);
+            console.log($geolocation.position.error);
+            console.log(position.error);
+
+            if(!$geolocation.position.error){
+
+
+
+
+                service.latitude = position.coords.latitude;
+                service.longitude = position.coords.longitude;
+                store.set('latitude', service.latitude);
+                store.set('longitude', service.longitude);
+            }
+
+
         },
 
         unsetLocation: function () {
@@ -20,28 +34,46 @@ app.factory('LocationService', function (store, $geolocation) {
             store.remove('longitude');
         },
 
-
-
         setCurrentPosition : function() {
-            $geolocation.getCurrentPosition();
-            $geolocation.watchPosition({
+
+            console.log("serCurrentPosition")
+/*
+            $geolocation.getCurrentPosition()
+                .then(function (position) {
+                    // This will be executed when the location is accessed
+                    service.setLocation(position);
+                    console.log("========== location found from $geolocation");
+                    console.log(position);
+                }, function (error) {
+                    // This will be executed if the user denies access
+                    // or the browser doesn't support the Geolocation API
+                    service.latitude = 46.778474;
+                    service.longitue = 6.641183;
+                    console.log("========== default location, based on Yverdon");
+                });
+*/
+           $geolocation.watchPosition({
                 timeout: 60000,
                 maximumAge: 250,
                 enableHighAccuracy: true
-            })
+            });
+           console.log($geolocation.position)
+           if(!$geolocation.position.error) {
+               service.setLocation($geolocation.position);
+           }
 
-        .then(function (position) {
-            // This will be executed when the location is accessed
-            service.setLocation(position);
-            console.log("========== location found from $geolocation");
-        }, function (error) {
-            // This will be executed if the user denies access
-            // or the browser doesn't support the Geolocation API
-            service.latitude = 46.778474;
-            service.longitue = 6.641183;
-            console.log("========== default location, based on Yverdon");
-        });
+            // this object updates regularly, it has 'error' property which is a 'truthy' and also 'code' and 'message' property if an error occurs
+
+            //It has all the location data
+          //  '$scope.myPosition.coords'
+
+            //It's truthy and gets defined when error occurs
+          //  '$scope.myPosition.error'
         }
+
+
+
+
     };
 
     return service;
