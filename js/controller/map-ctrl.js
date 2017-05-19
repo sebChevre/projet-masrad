4a64aab485e19cbf28691c2a54e5b7d9cc7ab75e
gblanc <gregoire.blanc@heig-vd.ch>
2017-05-19 17:00:57 +0200

    Redesign

--- js/controller/map-ctrl.js
+++ js/controller/map-ctrl.js
@@ -1,17 +1,18 @@
 /*
   Controlleur de la map de l'application
  */
-app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, LocationService, ModalService, UtilsService, $stateParams, $state, $scope, $rootScope, $geolocation,API_ALL_ISSUES,API_NEW_ISSUES,API_ISSUES_TYPE) {
+app.controller('MapCtrl', function (IssuesService, AuthService, NotifyService, LocationService, ModalService, UtilsService, $stateParams, $state, $scope, $rootScope, $geolocation, API_ALL_ISSUES, API_NEW_ISSUES, API_ISSUES_TYPE) {
 
     var map = this;
     var issuesType = [];
+    var currentZoom = 17;
     $scope.complexResult = null;
     map.markers = [];
 
     map.center = {
         lat: 0,
         lng: 0,
-        zoom: 5
+        zoom: currentZoom
     };
 
     map.defaults = {
@@ -31,7 +32,7 @@ app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, Lo
 
     // Call the modal window
     $scope.showNewIssue = function (lat, lon) {
-         console.log("list of type=" + issuesType);
+        console.log("list of type=" + issuesType);
         ModalService.showModal({
             templateUrl: "templates/newIssue.html",
             controller: "ModalInstanceCtrl",
@@ -45,26 +46,20 @@ app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, Lo
         }).then(function (modal) {
             modal.element.modal();
             modal.close.then(function (result) {
-                $scope.complexResult = "Name: " + result.name 
-                  + ", latitude: " + result.latitude 
-                  + ", longitude:" + result.longitude
-                  + ", selectedType:" + result.selectedType;
+                $scope.complexResult = "Name: " + result.name
+                    + ", latitude: " + result.latitude
+                    + ", longitude:" + result.longitude
+                    + ", selectedType:" + result.selectedType;
                 if (UtilsService.isUndefinedOrNull(result) || UtilsService.isUndefinedOrNull(result.name) || UtilsService.isUndefinedOrNull(result.latitude) || UtilsService.isUndefinedOrNull(result.longitude) || UtilsService.isUndefinedOrNull(result.selectedType)) {
                     console.log("Cannot insert a new Issue, the content of result isnt valid.")
                 } else {
-                    map.addNewIssue(result);
                     console.log(result);
-                    IssuesService.insertNewIssue(result,API_NEW_ISSUES);
+                    map.addNewIssue(result);
+                    IssuesService.insertNewIssue(result, API_NEW_ISSUES);
                 }
-
             });
         });
-
     };
-
-
-
-
 
     // Icone par defaut, position actuelle
     var defaultIcon = {
@@ -87,9 +82,6 @@ app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, Lo
         shadowSize: [41, 41]
     };
 
-
-
-
     map.addNewIssue = function (result) {
         map.markers.push({
             name: result.name,
@@ -97,7 +89,6 @@ app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, Lo
             lng: result.longitude,
             icon: pointsIcon
         })
-
     }
 
 
@@ -109,52 +100,52 @@ app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, Lo
         map.markers.push({
             lat: lat,
             lng: lng,
-            icon:defaultIcon,
+            icon: defaultIcon,
             draggable: false,
-            focus:true,
+            focus: true,
+            zoom: currentZoom,
             message: '<h3>Vous êtes ici...</h3>'
         });
 
         map.center = {
             lat: lat,
             lng: lng,
-            zoom: 18
+            zoom: currentZoom
         };
     });
 
     var showIssues = function (issue) {
-
-
         var lng = issue.location.coordinates[0];
         var lat = issue.location.coordinates[1];
-
 
         map.markers.push({
             lat: lat,
             lng: lng,
-            icon:pointsIcon,
+            icon: pointsIcon,
             draggable: false,
-            focus:true,
-            message: '<h4>' + issue.description +'</h4>'
+            focus: true,
+            message: '<h4>' + issue.description + '</h4>'
         });
 
         map.center = {
             lat: lat,
             lng: lng,
-            zoom: 10
+            zoom: currentZoom
         };
     }
 
-    $rootScope.$on('showIssuesClicked',function (event,issue){
+    $rootScope.$on('showIssuesClicked', function (event, issue) {
         showIssues(issue);
     });
 
-    $rootScope.$on('issueTypeFound',function (event,issTypes){
+    $rootScope.$on('issueTypeFound', function (event, issTypes) {
         console.log('issueTypeFound=' + issTypes);
         issuesType = issTypes;
     });
 
-    $scope.$on('$destroy', function() {
+
+
+    $scope.$on('$destroy', function () {
         console.log('destroy home');
         userPositionListener(); // remove listener.
     });
@@ -163,19 +154,32 @@ app.controller('MapCtrl', function (IssuesService,AuthService, NotifyService, Lo
     $scope.$on('leafletDirectiveMap.leaflet-zone.click', function (event, args) {
         console.log("create new issue, event=" + event + ", list of type=" + issuesType);
         console.log('Map clicked at coordinates [' + args.leafletEvent.latlng.lat + ', ' + args.leafletEvent.latlng.lng + ']');
-        //alert('Map clicked at coordinates [' + args.leafletEvent.latlng.lat + ', ' + args.leafletEvent.latlng.lng + ']')
         $scope.showNewIssue(args.leafletEvent.latlng.lat, args.leafletEvent.latlng.lng);
 
     });
 
-
-
-
     // Event listener to react to user finishing dragging a marker
     $scope.$on('leafletDirectiveMarker.dragend', function (events, args) {
         console.log('Marker moved to coordinates [' + args.model.lat + ', ' + args.model.lng + ']');
-        //alert('Marker moved to coordinates [' + args.model.lat + ', ' + args.model.lng + ']')
     });
+
+    // Save the zoom level after a zoom-change
+    $scope.$on('leafletDirectiveMap.leaflet-zone.zoomend', function (events, args) {
+        console.log('Zoom Changed [args=' + map.center.zoom + ']');
+        currentZoom = map.center.zoom;
+    });
+
+   /*
+    function objToString(obj) {
+        var str = '';
+        for (var p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                str += p + '::' + obj[p] + '\n';
+            }
+        }
+        return str;
+    }
+    */
 
 });
 
