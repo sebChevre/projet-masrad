feea7a8c586bad986e609fa87e30b13975b1049b
gblanc5 <gregoire.blanc@heig-vd.ch>
2017-05-22 12:33:28 +0200

    use the correct rest url.

--- js/constant.js
+++ js/constant.js
@@ -1,5 +1,5 @@
 //citizen api
-var API_BASE_URI = 'https://comem-citizen-engagement.herokuapp.com/api';
+var API_BASE_URI = 'https://masrad-dfa-2017-d.herokuapp.com/api';
 app.constant('API_LOGIN_URL', API_BASE_URI + '/auth');
 app.constant('API_USER_URL', API_BASE_URI + '/users');
 app.constant('API_ALL_ISSUES', API_BASE_URI + '/issues?include=creator');
